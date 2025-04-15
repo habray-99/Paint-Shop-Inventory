605aab3274c0ec95e2244227de8fecb67e857a9f
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2025-04-15 09:23:13 +0545

    Update DocumentLayout and related files to reflect new model structure and paths; adjust assembly info version.

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb91816de65a488f978fe9871168c51661a9f6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfaf68ee7796088914a44c24e446b3a813b2df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
