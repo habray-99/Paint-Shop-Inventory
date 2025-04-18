b1768dd5afc8112349ff5ccdf22b9d7fd9eb3f5e
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2025-04-18 08:36:05 +0545

    Refactor InventoryStock model: Update property names and types for consistency and clarity

--- WebApplication1/Models/InventoryStock.cs
+++ WebApplication1/Models/InventoryStock.cs
@@ -1,26 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Models;
 
+[Index(nameof(ProductId), IsUnique = false)]
+[Index(nameof(PurchaseDateAd), IsUnique = false)]
 public class InventoryStock
 {
     [Key]
-    public int StockId { get; set; }
+    public int BatchId { get; set; }
 
     [ForeignKey("Product")]
     public int ProductId { get; set; }
 
-    [Required]
-    public DateTime PurchaseDate { get; set; }
-
-    [Required]
     public int Quantity { get; set; }
-
-    [Required]
+    
+    [Column(TypeName = "dateAD")]
+    public DateTime PurchaseDateAd { get; set; }
+    [Column(TypeName = "dateBS")]
+    [MaxLength(20)]
+    public string PurchaseDateBs { get; set; }
+    
     [Column(TypeName = "decimal(18,2)")]
-    public decimal UnitCost { get; set; } // Dynamic wholesale price per batch
-
-    // Navigation property
+    public decimal CostPrice { get; set; }
+    
+    // Navigation
     public Product Product { get; set; }
 }
