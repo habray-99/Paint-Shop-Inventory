606dc5d0c068a2212748c2ed59caa152d461fa69
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2025-04-18 08:21:27 +0545

    Refactor Product model: Update properties and indexing for improved clarity and performance

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -4,34 +4,38 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models;
 
-[Index(nameof(Name), IsUnique = false)] // Index for faster searching
-[Index(nameof(Company), IsUnique = false)] // Index for faster searching
+[Index(nameof(ShadeSlug)), isUnique=false]
+[Index(nameof(Company)), isUnique=false]
+[Index(nameof(Category)), isUnique=false]
 public class Product
 {
-    [Required]
-    [StringLength(100)]
-    public required string Name { get; set; }
-
     [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ProductId { get; set; }
 
-    [Required]
-    [EnumDataType(typeof(ProductCategory))]
-    public ProductCategory Category { get; set; } // Paint/Putty/Smart Care
+    [Required, MaxLength(100)]
+    public string Name { get; set; }
 
     [Required]
-    [EnumDataType(typeof(ProductVolume))]
-    public ProductVolume Volume { get; set; } // Using enum for standardized volumes
+    public ProductCategory Category { get; set; }
 
     [Required]
-    [StringLength(100)]
-    // [Index(IsUnique = false)] // Index for faster filtering by company
-    public required string Company { get; set; }
+    public ProductVolume Volume { get; set; }
 
-    // Navigation properties
-    public virtual ICollection<InventoryStock>? InventoryStocks { get; set; }
-    public virtual ICollection<OrderItem>? OrderItems { get; set; }
+    [Required, MaxLength(100)]
+    public string Company { get; set; }
+
+    [Required, MaxLength(50)]
+    public string ShadeSlug { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal WholesalePrice { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public decimal RetailPrice { get; set; }
+
+    // Navigation
+    public ICollection<InventoryStock> InventoryStocks { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
 }
 
 public enum ProductCategory
@@ -43,18 +47,23 @@ public enum ProductCategory
 
 public enum ProductVolume
 {
+    [Display(Name = "500ML")] MilliLiter500 = 500,
+    [Display(Name = "200ML")] MilliLiter200 = 200,
+    [Display(Name = "100ML")] MilliLiter100 = 100,
+    [Display(Name = "50ML")] MilliLiter50 = 50,
     [Display(Name = "1L")] Liter1 = 1,
     [Display(Name = "4L")] Liter4 = 4,
     [Display(Name = "10L")] Liter10 = 10,
     [Display(Name = "20L")] Liter20 = 20,
     [Display(Name = "40L")] Liter40 = 40,
-    [Display(Name = "500ML")] MilliLiter500 = 500,
-    [Display(Name = "200ML")] MilliLiter200 = 200,
-    [Display(Name = "100ML")] MilliLiter100 = 100,
-    [Display(Name = "50ML")] MilliLiter50 = 50,
-    [Display(Name = "1Kg")] Kg1 = 1,
-    [Display(Name = "5Kg")] Kg5 = 5,
-    [Display(Name = "10Kg")] Kg10 = 10,
-    [Display(Name = "20Kg")] Kg20 = 20,
-    [Display(Name = "40Kg")] Kg40 = 40
+    //[Display(Name = "1Kg")] Kg1 = 1,
+    //[Display(Name = "5Kg")] Kg5 = 5,
+    //[Display(Name = "10Kg")] Kg10 = 10,
+    //[Display(Name = "20Kg")] Kg20 = 20,
+    //[Display(Name = "40Kg")] Kg40 = 40
+    [Display(Name = "1Kg")] Kg1 = 1001,  // Different numbering scheme
+    [Display(Name = "5Kg")] Kg5 = 1005,
+    [Display(Name = "10Kg")] Kg10 = 1010,
+    [Display(Name = "20Kg")] Kg20 = 1020,
+    [Display(Name = "40Kg")] Kg40 = 1040
 }
