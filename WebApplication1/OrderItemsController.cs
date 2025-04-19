d7d5b8a5d5fd9a7197104e7767ab8f2d1806bee1
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2025-04-19 12:28:13 +0545

    Add OrderItems management with CRUD functionality and associated views

---
+++ WebApplication1/OrderItemsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class OrderItemsController : Controller
+    {
+        private readonly WebApplication1Context _context;
+
+        public OrderItemsController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderItems
+        public async Task<IActionResult> Index()
+        {
+            var webApplication1Context = _context.OrderItems.Include(o => o.Order).Include(o => o.Product);
+            return View(await webApplication1Context.ToListAsync());
+        }
+
+        // GET: OrderItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.OrderItems
+                .Include(o => o.Order)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Create
+        public IActionResult Create()
+        {
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId");
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Company");
+            return View();
+        }
+
+        // POST: OrderItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ItemId,OrderId,ProductId,Quantity,UnitPrice,ExciseDuty,VAT")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", orderItem.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Company", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.OrderItems.FindAsync(id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", orderItem.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Company", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ItemId,OrderId,ProductId,Quantity,UnitPrice,ExciseDuty,VAT")] OrderItem orderItem)
+        {
+            if (id != orderItem.ItemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderItemExists(orderItem.ItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "OrderId", orderItem.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Company", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.OrderItems
+                .Include(o => o.Order)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var orderItem = await _context.OrderItems.FindAsync(id);
+            if (orderItem != null)
+            {
+                _context.OrderItems.Remove(orderItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderItemExists(int id)
+        {
+            return _context.OrderItems.Any(e => e.ItemId == id);
+        }
+    }
+}
