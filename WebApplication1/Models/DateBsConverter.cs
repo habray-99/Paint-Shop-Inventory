1a23bab29968649c9ce1a9a5bcbcd369d3ca49a0
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2025-04-19 10:19:22 +0545

    Add DateBsConverter: Implement conversion methods for Nepali and English date formats

---
+++ WebApplication1/Models/DateBsConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using NepaliDateConverter.Net;
+
+namespace WebApplication1.Models;
+
+public class DateBsConverter
+{
+    public static DateConverter ToBs(DateConverter date) => DateConverter.ConvertToNepali(date.Year, date.Month, date.Day);
+    public static DateConverter ToAd(DateTime date) => DateConverter.ConvertToEnglish(date.Year, date.Month, date.Day);
+    
+}
